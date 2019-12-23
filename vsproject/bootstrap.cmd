b123b8e8c82312ab33b1f4320198b5f4d333a1a4
Arlo Siemsen <arkixml@gmail.com>
2019-12-23 08:01:03 -0800

    Update build scripts

--- vsproject/bootstrap.cmd
+++ vsproject/bootstrap.cmd
@@ -25,6 +25,8 @@ copy %OUTDIR%rustc-build\rustc_binary.exe %BINDIR_S%rustc.exe
 copy %OUTDIR%rustc-build\rustc_binary.exe %BINDIR%rustc_m.exe
 copy %OUTDIR%cargo-build\cargo.exe %BINDIR%\cargo.exe
 
+goto :skip1
+
 @REM Hard-coded build of libstd
 %BINDIR_S%rustc --crate-type rlib --crate-name core -L %LIBDIR_S% %RUST_SRC%libcore/lib.rs -o %LIBDIR_S%libcore.rlib
 %BINDIR_S%rustc --crate-type rlib --crate-name compiler_builtins -L %LIBDIR_S% %RUST_SRC%libcompiler_builtins/src/lib.rs -o %LIBDIR_S%libcompiler_builtins.rlib --cfg feature=\"compiler-builtins\"
@@ -35,6 +37,8 @@ copy %OUTDIR%cargo-build\cargo.exe %BINDIR%\cargo.exe
 %BINDIR_S%rustc --crate-type rlib --crate-name panic_unwind -L %LIBDIR_S% %RUST_SRC%libpanic_unwind/lib.rs -o %LIBDIR_S%libpanic_unwind.rlib
 %BINDIR_S%rustc --crate-type rlib --crate-name std -L %LIBDIR_S% %RUST_SRC%libstd/lib.rs -o %LIBDIR_S%libstd.rlib -l kernel32 -l shell32 -l msvcrt -l userenv -l ws2_32 -l advapi32
 @if %errorlevel% neq 0 exit /b %errorlevel%
+
+:skip1
 
 set CARGO_CONFIG=%CARGO_HOME%config
 echo [source.crates-io] > %CARGO_CONFIG%
@@ -55,7 +59,7 @@ copy /y %OUTDIR%build-std\release\deps\*.dll* %LIBDIR%
 mkdir %OUTDIR%build-test
 set CARGO_TARGET_DIR=%OUTDIR%build-test
 set RUSTC=%BINDIR%rustc_m 
-%BINDIR%cargo build --manifest-path %RUST_SRC%libtest/Cargo.toml  -j 1 --release
+%BINDIR%cargo build --manifest-path %RUST_SRC%libtest/Cargo.toml -j 1 --release
 @if %errorlevel% neq 0 exit /b %errorlevel%
 copy /y %OUTDIR%build-test\release\deps\*.rlib %LIBDIR%
 copy /y %OUTDIR%build-test\release\deps\*.dll* %LIBDIR%
@@ -66,15 +70,17 @@ set TMPDIR=%PREFIX%\tmp
 set CARGO_TARGET_DIR=%OUTDIR%build-rustc
 set RUSTC=%BINDIR%rustc_m
 set RUSTC_ERROR_METADATA_DST=%PREFIX%
-set CFG_RELEASE=
-set CFG_RELEASE_CHANNEL=
+set CFG_RELEASE=%RUSTC_VERSION%
+set CFG_RELEASE_CHANNEL=stable
 set CFG_VERSION=%RUSTC_VERSION%-stable-mrustc
 set CFG_PREFIX=mrustc
 set CFG_LIBDIR_RELATIVE=lib
 mkdir %TMPDIR%
 mkdir %CARGO_TARGET_DIR%
-%BINDIR%cargo build --manifest-path %RUST_SRC%rustc/Cargo.toml --release -j 1
+%BINDIR%cargo build --manifest-path %RUST_SRC%rustc/Cargo.toml --release -j 1 --verbose
 @if %errorlevel% neq 0 exit /b %errorlevel%
 copy /y %LIBDIR%*.dll %BINDIR%
+copy /y %OUTDIR%build-rustc\release\deps\*.rlib %LIBDIR%
+copy /y %OUTDIR%build-rustc\release\deps\*.dll* %LIBDIR%
 copy /y %OUTDIR%build-rustc\release\deps\*.dll %BINDIR%
 copy /y %OUTDIR%build-rustc\release\rustc_binary.exe %BINDIR%rustc_binary.exe
--- vsproject/build_rustc_minicargo.cmd
+++ vsproject/build_rustc_minicargo.cmd
@@ -2,6 +2,7 @@
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
 @mkdir %OUTDIR%\rustc-build
+set CFG_RELEASE=%RUSTC_VERSION%
 set CFG_VERSION=%RUSTC_VERSION%-stable-mrustc
 x64\Release\minicargo.exe ..\rustc-%RUSTC_VERSION%-src\src\rustc -L %OUTDIR% --output-dir %OUTDIR%\rustc-build --vendor-dir ..\rustc-%RUSTC_VERSION%-src\src\vendor
 @if %errorlevel% neq 0 exit /b %errorlevel%
