20054cb59eaf43db4734d9c23f00a47a3a4094f4
Arlo Siemsen <arkixml@gmail.com>
2020-01-02 15:00:27 -0800

    cleanup

--- src/expand/format_args.cpp
+++ src/expand/format_args.cpp
@@ -200,9 +200,9 @@ namespace {
         ::std::string   cur_literal;
 
         const char* s = format_string.c_str();
-        const char* s_end = s + format_string.length();
+        const char* const s_end = s + format_string.length();
 
-        for( ; s != s_end; s ++)
+        for( ; s < s_end; s ++)
         {
             if( *s != '{' )
             {
@@ -231,7 +231,7 @@ namespace {
 
                 // Debugging: A view of the formatting fragment
                 const char* s2 = s;
-                while(*s2 && *s2 != '}')
+                while(s2 < s_end && *s2 != '}')
                     s2 ++;
                 auto fmt_frag_str = string_view { s, s2 };
 
