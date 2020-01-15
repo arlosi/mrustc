9f516dd8e3f8146bf03f9f4f5014ada5d21a967e
Arlo Siemsen <arkixml@gmail.com>
2020-01-15 09:27:12 -0800

    add bootstrap next

---
+++ vsproject/bootstrap-next.cmd
@@ -0,0 +1,25 @@
+@setlocal
+@if %errorlevel% neq 0 exit /b %errorlevel%
+
+set WORKDIR=%~dp0rustc_bootstrap\
+set RUSTC_VERSION_NEXT=1.30.0
+set PREFIX=%~dp0\output-1.29.0\prefix\
+
+curl -O https://static.rust-lang.org/dist/rustc-%RUSTC_VERSION_NEXT%-src.tar.gz
+
+echo --- Working in directory %WORKDIR%
+echo === Cleaning up
+rmdir /s/q %WORKDIR%build
+
+echo === Building rustc bootstrap mrustc stage0
+mkdir %WORKDIR%mrustc
+tar -xzf rustc-%RUSTC_VERSION_NEXT%-src.tar.gz -C %WORKDIR%mrustc/
+
+echo [build]> %WORKDIR%mrustc\rustc-%RUSTC_VERSION_NEXT%-src\config.toml
+echo cargo = "%PREFIX:\=/%bin/cargo.exe">> %WORKDIR%mrustc\rustc-%RUSTC_VERSION_NEXT%-src\config.toml
+echo rustc = "%PREFIX:\=/%bin/rustc_binary.exe">> %WORKDIR%mrustc\rustc-%RUSTC_VERSION_NEXT%-src\config.toml
+echo full-bootstrap = true>> %WORKDIR%mrustc\rustc-%RUSTC_VERSION_NEXT%-src\config.toml
+echo vendor = true>> %WORKDIR%mrustc\rustc-%RUSTC_VERSION_NEXT%-src\config.toml
+
+echo "--- Running x.py, see %WORKDIR%mrustc.log for progress"
+@REM cd %WORKDIR%mrustc\rustc-%RUSTC_VERSION_NEXT%-src && python ./x.py build --stage 3 > %WORKDIR%mrustc.log 2>&1
